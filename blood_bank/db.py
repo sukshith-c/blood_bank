1b2c68a3dd151e275fe11870ced0df5b22799017
sukshith-c <sukshithc24cs@rnsit.ac.in>
2026-05-05 01:19:06 +0530

    final updated version

--- blood_bank/db.py
+++ blood_bank/db.py
@@ -1,7 +1,10 @@
+import os
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
-client = MongoClient("mongodb+srv://sukshithc24cs_db_user:Sukshith123@cluster-1.jjhajhp.mongodb.net/")
+load_dotenv()
+
+client = MongoClient(os.getenv("MONGO_URI"))
 
 db = client["blood_bank_system"]
-
 collection = db["blood_inventory"]
